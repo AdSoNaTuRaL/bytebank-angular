13932743948920f79df25764e03dae11ec078dad
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-18 21:20:58 +0200

    add new methods and typing data

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,12 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  @Output() valoresComErro = new EventEmitter<string>();
+
   valor!: number;
   destino!: string;
 
-  transferir() {
-    console.log('Teste');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+  private isValido(): boolean {
+    const valido = this.valor > 0;
+
+    if (!valido) {
+      this.valoresComErro.emit('Informe um valor valido!');
+      this.limparCampos();
+    }
+
+    return valido;
+  }
+
+  transferir(): void {
+    if (this.isValido()) {
+      const valorEmitir = { valor: this.valor, destino: this.destino };
+      this.aoTransferir.emit(valorEmitir);
+      this.limparCampos();
+    }
+  }
+
+  limparCampos(): void {
+    this.valor = 0;
+    this.destino = '';
   }
 }
