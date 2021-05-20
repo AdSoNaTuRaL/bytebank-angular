45d69845b7ddd0070580099d4cf62e7ccd22b57f
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-20 13:16:41 +0200

    refactor methods on transferencia service for use httpClient (angular) on requests and use Transferencia Model

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,32 +1,30 @@
+import { Transferencia } from './../../models/transferencia.model';
 import { Injectable } from '@angular/core';
-
-interface ITransferencia {
-  data?: Date;
-  valor?: number;
-  destino?: string;
-}
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
-  private transferencias: ITransferencia [];
+  private transferencias: Transferencia [];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpCliente: HttpClient) {
     this.transferencias = [];
   }
 
-  get ListOftransferencia(): ITransferencia[] {
-    return this.transferencias;
+  all(): Observable<Transferencia[]> {
+    return this.httpCliente.get<Transferencia[]>(this.url);
   }
 
-  adicionarTransferencia(transferencia: ITransferencia): void {
+  adicionarTransferencia(transferencia: Transferencia): Observable<Transferencia> {
     this.hidratar(transferencia);
 
-    this.transferencias.push(transferencia);
+    return this.httpCliente.post<Transferencia>(this.url, transferencia);
   }
 
-  private hidratar(transferencia: ITransferencia): void {
+  private hidratar(transferencia: Transferencia): void {
     transferencia.data = new Date();
   }
 }
